6e5941f5cdda1f78716caac5da17314838ddabeb
Juan Daniel López Serna <jjuanda@gmail.com>
2015-12-24 09:44:06 +0100

    keys can now be ignored

--- dict_differ/DictDiffer.py
+++ dict_differ/DictDiffer.py
@@ -8,10 +8,11 @@ class DictDiffer(object):
         super(DictDiffer, self).__init__()
         self.dbase = dbase
         self.dnew  = dnew
+        self.ccmp = None
     
-    def compare(self):
-        kbase = set(self.dbase.keys())
-        knew  = set(self.dnew.keys())
+    def compare(self, ign_keys=[]):
+        kbase = set(self.dbase.keys()) - set(ign_keys)
+        knew  = set(self.dnew.keys()) - set(ign_keys)
 
         common_keys  = kbase.intersection(knew)
         new_keys     = knew - kbase
@@ -22,23 +23,48 @@ class DictDiffer(object):
 
         equal_values = dict([(k, self.dbase[k]) for k in common_keys if k not in diff_values])
 
-        return {
+        self.ccmp = {
             'new'     : dict([(k, self.dnew[k]) for k in new_keys]),
             'removed' : dict([(k, self.dbase[k]) for k in removed_keys]),
             'equal'   : equal_values,
             'diff'    : diff_values,
         }
 
+        return self.ccmp
+
     def equal(self):
-        c = self.compare()
-        return (len(c['new']) == 0) and (len(c['removed']) == 0) and (len(c['diff']))
+        c = self.ccmp
+        if c is None:
+            c = self.compare()
+        return (len(c['new']) == 0) and (len(c['removed']) == 0) and (len(c['diff']) == 0)
 
     def diff_vals(self, vbase, vnew):
-        if type(vbase) != vnew:
+        if type(vbase) != type(vnew):
             return True
-
-        if type(vbase) is dict:
+        elif type(vbase) is dict:
             return not (DictDiffer(vbase, vnew).equal())
         else:
             return not (vbase == vnew)
 
+
+
+if __name__ == '__main__':
+    from bson.objectid import ObjectId
+    import datetime
+    from pprint import pprint
+
+    qq = [{'oldid': ObjectId('54b4d42655067b0b926a5c83'),
+  'dt': datetime.datetime(2014, 5, 14, 0, 0),
+  'nav': '27.71',
+  'isin': 'NL0000289783',
+  '_id': ObjectId('55eaddd455067ba9a2545b56'),
+  'dt_created': datetime.datetime(2014, 5, 14, 17, 9, 56, 890000)},
+ {'oldid': ObjectId('54b4d42655067b0b926a5c84'),
+  'dt': datetime.datetime(2014, 5, 14, 0, 0),
+  'nav': '27.71',
+  'isin': 'NL0000289783',
+  '_id': ObjectId('55eaddd455067ba9a2545b57'),
+  'dt_created': datetime.datetime(2014, 5, 14, 17, 9, 56, 890000)}]
+
+    dd=DictDiffer(qq[0], qq[1])
+    pprint(dd.compare()) 
